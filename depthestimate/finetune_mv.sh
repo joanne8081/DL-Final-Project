b5d7ef543edd932b806d7a44fa2044be01af8ded
phuang0713 <SleepyboyHuang@HuangPo-HandeMacBook-Pro.local>
2017-11-21 06:16:10 -0800

    finetune for mv

--- depthestimate/finetune_mv.sh
+++ depthestimate/finetune_mv.sh
@@ -2,15 +2,44 @@
 DATAFILE=$1
 DUMPFILE=$2
 PKLFILE=$3
-BN0=$4
-RATE=$6
-PKLFILE2="${PKLFILE}/twobranch_nn_mv.pkl"  
+PSFILE=$4
+BN0=$5
+EPOCH=$6
+RATE=$7
+TESTSIZE=$8
+PKLFILE2="${PKLFILE}/twobranch_nn_mv.pkl"
 
-xvfb-run -a python nn_mv.py data=$DATAFILE dump=$DUMPFILE pkl=../demo/twobranch_v2.pkl lr=$6 bno=$4 mvfinetune
-for number in `seq 1 $5`; 
+echo =================================Epoch 0===================================
+xvfb-run -a python nn_mv.py data=$DATAFILE dump=$DUMPFILE pkl=../demo/twobranch_v2.pkl lr=$7 bno=$5 mvfinetune >> garbage.txt
+for number in `seq 1 $6`; 
 do
+echo =================================Epoch $number===================================
 value=`echo $6 | sed -e 's/[eE]+*/\\*10\\^/'`
-learning_rate=`echo "$value*((0.96)^$number)" | bc -l`
-xvfb-run -a python nn_mv.py model=$DUMPFILE dump=$PKLFILE lr=$learning_rate exportpkl
-xvfb-run -a python nn_mv.py data=$DATAFILE dump=$DUMPFILE pkl=$PKLFILE2 bno=$4 lr=$learning_rate mvfinetune
+learning_rate=`echo "$value*((0.99)^$number)" | bc -l`
+xvfb-run -a python nn_mv.py model=$DUMPFILE dump=$PKLFILE lr=$learning_rate exportpkl >> garbage.txt
+xvfb-run -a python nn_mv.py data=$DATAFILE dump=$DUMPFILE pkl=$PKLFILE2 bno=$5 lr=$learning_rate mvfinetune >> garbage.txt
+tarn=$((number%5))
+if [ "$tarn" -eq 0 ]; then
+	TARFILE="${PSFILE}_ep${number}.tar.gz"
+	PSFILE2="${PSFILE}_cp"
+	mkdir $PSFILE2
+	echo Do Test and Copy to $PSFILE2 and Tar to $TARFILE
+	xvfb-run -a python nn_mv.py data=$DATAFILE model=$DUMPFILE dump=$PSFILE num=$8 test >> garbage.txt
+	testsize=`echo "$8 - 1" | bc`
+	echo $textsize
+	for n in `seq 0 $testsize`;
+	do
+		for num in `seq 1 6 29`;
+		do
+		cmd="cp ${PSFILE}/pts_${n}_${num}.txt $PSFILE2"
+		$cmd
+		done
+	done
+	cmd="cp $PKLFILE2 $PSFILE2"
+	$cmd
+	tar zcvf $TARFILE $PSFILE2 >> garbage.txt
+	rm -rf $PSFILE2
+fi
+rm -rf garbage.txt
 done
+
