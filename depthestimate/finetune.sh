e3e23e8e33599e930ec0e3cf7dbe2698ba4a1ab7
phuang0713 <SleepyboyHuang@HuangPo-HandeMacBook-Pro.local>
2017-11-21 01:51:14 -0800

    filetune.sh patch

--- depthestimate/finetune.sh
+++ depthestimate/finetune.sh
@@ -6,11 +6,11 @@ BN0=$4
 RATE=$6
 PKLFILE2="${PKLFILE}/twobranch_nn.pkl"  
 
-#xvfb-run -a python nn.py data=$DATAFILE dump=$DUMPFILE pkl=../demo/twobranch_v2.pkl bno=$4 finetune
+xvfb-run -a python nn.py data=$DATAFILE dump=$DUMPFILE pkl=../demo/twobranch_v2.pkl lr=$6 bno=$4 finetune
 for number in `seq 1 $5`; 
 do
 value=`echo $6 | sed -e 's/[eE]+*/\\*10\\^/'`
-learning_rate=`echo "$value/$number" | bc -l`
-#xvfb-run -a python nn.py model=$DUMPFILE dump=$PKLFILE exportpkl
-#xvfb-run -a python nn.py data=$DATAFILE dump=$DUMPFILE pkl=$PKLFILE2 bno=$4 finetune
+learning_rate=`echo "$value*((0.96)^$number)" | bc -l`
+xvfb-run -a python nn.py model=$DUMPFILE dump=$PKLFILE lr=$learning_rate exportpkl
+xvfb-run -a python nn.py data=$DATAFILE dump=$DUMPFILE pkl=$PKLFILE2 bno=$4 lr=$learning_rate finetune
 done
