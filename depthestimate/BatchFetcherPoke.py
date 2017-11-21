9bd316883ba6730e32fea3af07ff8b5dda9e4499
kuanwenhuang <kuanwenh@usc.edu>
2017-11-21 02:07:37 -0800

    Renderer folder created

--- depthestimate/BatchFetcherPoke.py
+++ depthestimate/BatchFetcherPoke.py
@@ -15,8 +15,9 @@ import show3d
 from PIL import Image
 
 NUM_VIEW = 5
-FETCH_BATCH_SIZE=30
-BATCH_SIZE=30
+TOTAL_NUM_VIEW = 30
+FETCH_BATCH_SIZE=100
+BATCH_SIZE=100
 HEIGHT=192
 WIDTH=256
 POINTCLOUDSIZE=4096
@@ -51,63 +52,20 @@ class BatchFetcher(threading.Thread):
 		return data, ptcloud
 		
 	def work(self,bno):
-
-		'''		
-		path = os.path.join(self.datadir,'%d/%d.gz'%(bno//1000,bno))
-		if not os.path.exists(path):
-			self.stopped=True
-			print "error! data file not exists: %s"%path
-			print "please KILL THIS PROGRAM otherwise it will bear undefined behaviors"
-			assert False,"data file not exists: %s"%path
-		binfile=zlib.decompress(open(path,'r').read())
-		p=0
-
-
-		color=np.fromstring(binfile[p:p+FETCH_BATCH_SIZE*HEIGHT*WIDTH*3],dtype='uint8').reshape((FETCH_BATCH_SIZE,HEIGHT,WIDTH,3))
-		p+=FETCH_BATCH_SIZE*HEIGHT*WIDTH*3
-		depth=np.fromstring(binfile[p:p+FETCH_BATCH_SIZE*HEIGHT*WIDTH*2],dtype='uint16').reshape((FETCH_BATCH_SIZE,HEIGHT,WIDTH))
-		p+=FETCH_BATCH_SIZE*HEIGHT*WIDTH*2
-		rotmat=np.fromstring(binfile[p:p+FETCH_BATCH_SIZE*3*3*4],dtype='float32').reshape((FETCH_BATCH_SIZE,3,3))
-		p+=FETCH_BATCH_SIZE*3*3*4
-		ptcloud=np.fromstring(binfile[p:p+FETCH_BATCH_SIZE*POINTCLOUDSIZE*3],dtype='uint8').reshape((FETCH_BATCH_SIZE,POINTCLOUDSIZE,3))
-		ptcloud=ptcloud.astype('float32')/255
-
-
-		beta=math.pi/180*20
-		viewmat=np.array([[
-			np.cos(beta),0,-np.sin(beta)],[
-			0,1,0],[
-			np.sin(beta),0,np.cos(beta)]],dtype='float32')
-		rotmat=rotmat.dot(np.linalg.inv(viewmat))
-		for i in xrange(FETCH_BATCH_SIZE):
-			ptcloud[i]=((ptcloud[i]-[0.7,0.5,0.5])/0.4).dot(rotmat[i])+[1,0,0]
-		p+=FETCH_BATCH_SIZE*POINTCLOUDSIZE*3
-		reeb=np.fromstring(binfile[p:p+FETCH_BATCH_SIZE*REEBSIZE*2*4],dtype='uint16').reshape((FETCH_BATCH_SIZE,REEBSIZE,4))
-		p+=FETCH_BATCH_SIZE*REEBSIZE*2*4
-		keynames=binfile[p:].split('\n')
-		reeb=reeb.astype('float32')/65535
-
-
-		for i in xrange(FETCH_BATCH_SIZE):
-			reeb[i,:,:3]=((reeb[i,:,:3]-[0.7,0.5,0.5])/0.4).dot(rotmat[i])+[1,0,0]
-		data=np.zeros((FETCH_BATCH_SIZE,HEIGHT,WIDTH,4),dtype='float32')
-		data[:,:,:,:3]=color*(1/255.0)
-		data[:,:,:,3]=depth==0
-		validating=np.array([i[0]=='f' for i in keynames],dtype='float32')
-		'''
 		datalist = os.listdir(self.datadir)
 		datalist = [x for x in datalist if x!='0']
-
-		data=np.zeros((FETCH_BATCH_SIZE,HEIGHT,WIDTH,4),dtype='float32')
-		ptcloud=np.zeros((FETCH_BATCH_SIZE,POINTCLOUDSIZE,3),dtype='float32')		
+		data=np.zeros((FETCH_BATCH_SIZE,NUM_VIEW,HEIGHT,WIDTH,4),dtype='float32')
+		ptcloud=np.zeros((FETCH_BATCH_SIZE,NUM_VIEW,POINTCLOUDSIZE,3),dtype='float32')		
 		validating = np.random.randint(16,size=FETCH_BATCH_SIZE)==0
 		for i in range(FETCH_BATCH_SIZE):
-			pokenum = "{0:03d}".format(bno+1)
-			path2png = os.path.join(self.datadir, pokenum, pokenum+'_{}.png'.format(i))
-			path2txt = os.path.join(self.datadir, pokenum, pokenum+'.txt')
-			single_data, single_ptcloud=self.fetch_single(path2png, path2txt)
-			data[i,:,:,:] = single_data
-			ptcloud[i,:,:] = single_ptcloud
+			pokenum = datalist[bno]
+			viewnum = random.sample(range(TOTAL_NUM_VIEW), NUM_VIEW)
+			for j in range(len(viewnum)):	
+				path2png = os.path.join(self.datadir, pokenum, pokenum+'_{}.png'.format(viewnum[j]))
+				path2txt = os.path.join(self.datadir, pokenum, pokenum+'.txt')
+				single_data, single_ptcloud=self.fetch_single(path2png, path2txt)
+				data[i,j,:,:,:] = single_data
+				ptcloud[i,j,:,:] = single_ptcloud
  
 		return (data,ptcloud,validating)
 
@@ -126,7 +84,7 @@ class BatchFetcher(threading.Thread):
 			self.queue.get()
 
 if __name__=='__main__':
-	dataname = "YTTRBtraindump_220k"
+	dataname = "YTTRBtraindump_220k" #default weird name..
 	fetchworker = BatchFetcher(dataname)
 	fetchworker.bno=0
 	fetchworker.start()
