e6c473a120dfde1cd631d857255ef088fc0c58b2
kuanwenhuang <kuanwenh@usc.edu>
2017-11-27 06:18:07 -0800

    script for LSTM2

--- depthestimate/finetune_mvlstm2.sh
+++ depthestimate/finetune_mvlstm2.sh
@@ -9,15 +9,21 @@ RATE=$7
 TESTSIZE=$8
 PKLFILE2="${PKLFILE}/twobranch_nnmvlstm2.pkl"
 
+xvfb-run -a python nnmvlstm2.py data=../data dump=finetunelstm2 pkl=../demo/twobranch_v2.pkl lr=3e-5 bno=120  mvfinetune
+
+xvfb-run -a python nnmvlstm2.py model=finetunelstm2 dump=pkldumplstm3 lr=3e-5 exportpkl
+
+xvfb-run -a python nnmvlstm2.py data=../data dump=finetunelstm2 pkl=./pkldumplstm3/twobranch_nnmvlstm2.pkl lr=3e-5 bno=120 mvfinetune
+
 echo =================================Epoch 0===================================
-xvfb-run -a python nnmvlstm.py data=$DATAFILE dump=$DUMPFILE pkl=./pkldumplstm3/twobranch_nnmvlstm2.pkl lr=$7 bno=$5 mvfinetune
+xvfb-run -a python nnmvlstm2.py data=$DATAFILE dump=$DUMPFILE pkl=./pkldumplstm3/twobranch_nnmvlstm2.pkl lr=$7 bno=$5 mvfinetune
 for number in `seq 1 $6`; 
 do
 echo =================================Epoch $number===================================
 value=`echo $7 | sed -e 's/[eE]+*/\\*10\\^/'`
 learning_rate=`echo "$value*((0.99)^$number)" | bc -l`
-xvfb-run -a python nnmvlstm.py model=$DUMPFILE dump=$PKLFILE lr=$learning_rate exportpkl
-xvfb-run -a python nnmvlstm.py data=$DATAFILE dump=$DUMPFILE pkl=$PKLFILE2 bno=$5 lr=$learning_rate mvfinetune
+xvfb-run -a python nnmvlstm2.py model=$DUMPFILE dump=$PKLFILE lr=$learning_rate exportpkl
+xvfb-run -a python nnmvlstm2.py data=$DATAFILE dump=$DUMPFILE pkl=$PKLFILE2 bno=$5 lr=$learning_rate mvfinetune
 tarn=$((number%5))
 if [ "$tarn" -eq 0 ]; then
 	TARFILE="${PSFILE}_ep${number}.tar.gz"
